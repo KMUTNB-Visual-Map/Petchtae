e854d49afdf40b3a2834f8943dc95144fab7f180
s67-Petchtae <s6701012610139@email.kmutnb.ac.th>
2026-02-19 15:56:59 +0700

    Delete avatar_rendering.ts

--- avatar_rendering.ts
+++
@@ -1,9 +0,0 @@
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-function loadAvatar(scene, url: string) {
-  const loader = new GLTFLoader();
-  loader.load(url, gltf => {
-    const model = gltf.scene;
-    scene.add(model);
-  });
-}
